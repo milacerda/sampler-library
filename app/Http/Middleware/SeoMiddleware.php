cc3d4c68a4d650491fbaeab5ea45c2da68e2fffe
Camila Lacerda <milacerda@gmail.com>
2021-05-31 13:26:31 -0300

    view

--- app/Http/Middleware/SeoMiddleware.php
+++ app/Http/Middleware/SeoMiddleware.php
@@ -29,7 +29,7 @@ class SeoMiddleware
             // switch (Route::getCurrentRoute()->getPath()) {
                 // case "tournaments/{tournament}/register":
                     // $tournament = Tournament::where('slug', $request->tournament)->first();
-                    return view('public/static');
+                    return view('static');
             // }
         }
         return $next($request);
